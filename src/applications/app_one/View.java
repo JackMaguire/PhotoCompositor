a52542223c330347ffef4a318305470f0830f259
Jack Maguire <jackmaguire1444@gmail.com>
2018-03-29 10:16:24 -0400

    added plan for view

---
+++ src/applications/app_one/View.java
@@ -0,0 +1,50 @@
+package applications.app_one;
+
+public class View {
+
+	/*   Plan:
+	 * 
+	 * 		.------------------------------.
+	 * 		|Tabs	|												|
+	 * 		|-----	|												|
+	 * 		|			|				Composite				|
+	 * 		|			|				 Picture					|
+	 * 		|			|												|
+	 * 		|			|												|
+	 * 		.------------------------------.
+	 * 
+	 * 
+	 * 	Tabs:
+	 * 
+	 * 	.-------------.
+	 *  |			IO			  |
+	 *	  |------------	|
+	 *  |	Save File		|
+	 *  | Save Session	|
+	 *  | Load Session	|
+	 * 
+	 * 
+	 * 	.-----------------.
+	 *  |		 Weights 		  |
+	 *	  |-----------------	|
+	 *  |	Slider for pic1 	|
+	 *  |	Slider for pic2 	|
+	 *  |	Slider for pic3 	|
+	 *  ...
+	 *  |	Slider for picN 	|
+	 *  | ---
+	 *  | JComboBox for Formulas (Linear, exponential decay, etc)
+	 *  | Options for Formula
+	 *  | "Apply" JButton |
+	 * 
+	 * 
+	 * 	.-------------.
+	 * 	|  ImageType  | <- Using JCheckBoxs instead of JComboBox in case two options are compatible
+	 * 	|-------------|
+	 * 	| JCheckBox for Weighted
+	 * 	| JCheckBox for Mean
+	 * 	| JCheckBox for RMSF
+	 * 
+	 */
+
+}
