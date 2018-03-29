891aa051a555eccaea2be9c208b2206bb8828df6
Jack Maguire <jackmaguire1444@gmail.com>
2018-03-29 14:46:53 -0400

    main() does input checking now

--- src/applications/app_one/AppOne.java
+++ src/applications/app_one/AppOne.java
@@ -5,6 +5,12 @@ import java.awt.image.BufferedImage;
 public class AppOne {
 
 	public static void main(String[] args) {
+		
+		if( args.length < 2 ){
+			System.out.println("Every argument should be the path to an image file.");
+			return;
+		}
+		
 		BufferedImage[] images = new BufferedImage[args.length];
 		for (int i = 0; i < args.length; ++i) {
 			images[ i ] = util.PhotoIO.imageFromFile( args[i] );
